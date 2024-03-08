ceea77d7fa0157f96fa0116e1f3b5d5ee20d0fc3
hamid811394 <152581568+hamid811394@users.noreply.github.com>
2024-03-09 02:01:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 28.592071, Ing: 77.219592},
+        zoom: 18,
+        mapId: '4894e6502b7e4ebb '
+    });
+} 
